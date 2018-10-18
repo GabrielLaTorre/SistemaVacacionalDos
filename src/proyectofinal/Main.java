2aa9e61ff561de4ec0be405dac5457d125dbafaa
Gabe <lt.gabriel96@gmail.com>
2018-10-17 22:35:46 -0300

    Proyecto de Sistema Vacacional2

---
+++ src/proyectofinal/Main.java
@@ -0,0 +1,15 @@
+package proyectofinal;
+//Realizando un proyecto que combine el Sistema vacacional con las intefaces de Java
+//En curso....
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Bienvenida bienvenida1 = new Bienvenida();
+        bienvenida1.setBounds(0, 0, 350, 450);
+        bienvenida1.setVisible(true);
+        bienvenida1.setResizable(false);
+        bienvenida1.setLocationRelativeTo(null);                
+    }
+    
+}
